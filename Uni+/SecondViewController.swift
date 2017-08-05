37d36ee46c542c538089bd52d4304312c088e822
Chase Liu <shuyangliu@Chases-MacBook-Air.local>
2017-08-05 11:37:09 +1000

    Initial Commit

---
+++ Uni+/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Uni+
+//
+//  Created by Chase Liu on 5/8/17.
+//  Copyright © 2017 Chase Liu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
