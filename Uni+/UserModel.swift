3efb9bbad30467a6433eff111e3cf4b024055f9e
rmit-s3610584-Shuyang-Liu <s3610584@student.rmit.edu.au>
2017-09-29 12:59:26 +1000

    state

---
+++ Uni+/UserModel.swift
@@ -0,0 +1,30 @@
+//
+//  NoteModel.swift
+//  Uni+
+//
+//  Created by Chase on 27/8/17.
+//  Copyright © 2017 Chase Liu. All rights reserved.
+//
+
+import Foundation
+
+
+class User{
+
+
+
+
+
+}
+    
+
+
+    
+
+
+    
+
+
+
+
+
